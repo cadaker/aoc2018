993a5ebd099d03bbc9843e3043683fae1171f6d6
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-02 13:31:43 +0100

    Leiningen project file.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject aoc2018 "0.0.0"
+  :description "Clojure solutions for Advent of Code 2018"
+  :url "https://github.com/cadaker/aoc2018"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main aoc2018.core
+  :profiles {:uberjar {:aot :all}})
