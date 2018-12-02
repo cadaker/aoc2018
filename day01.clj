546ee0e0e94ed6b78f77490f6d98991fd1300915
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-02 01:52:10 +0100

    Simplify a bit.

--- day01.clj
+++ day01.clj
@@ -1,13 +1,11 @@
 (ns day01)
 
 (defn find-freq-reached-twice [in-diffs]
-  (loop [freq 0, diffs in-diffs, seen #{0}]
-    (if (seq diffs)
-      (let [freq' (+ freq (first diffs))]
-        (if (contains? seen freq')
-          freq'
-          (recur freq' (rest diffs) (conj seen freq'))))
-      (recur freq in-diffs seen))))
+  (loop [freq 0, diffs (cycle in-diffs), seen #{0}]
+    (let [freq' (+ freq (first diffs))]
+      (if (contains? seen freq')
+        freq'
+        (recur freq' (rest diffs) (conj seen freq'))))))
 
 (defn -main []
   (let [diffs (map #(Long/valueOf %) (line-seq (java.io.BufferedReader. *in*)))
