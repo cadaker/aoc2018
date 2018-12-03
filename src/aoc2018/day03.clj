0a96ac942059a76c1ffd8c6e4ef2dfbbfcbd112d
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-03 23:45:55 +0100

    Day 3 part 2.

--- src/aoc2018/day03.clj
+++ src/aoc2018/day03.clj
@@ -3,25 +3,31 @@
 
 (defn get-rect [input-match]
   (let [entry #(Integer/valueOf (nth input-match %))]
-    {:x (entry 2)
+    {:id (entry 1)
+     :x (entry 2)
      :y (entry 3)
      :w (entry 4)
      :h (entry 5)}))
 
+(defn rect-coords [rect]
+  (for [x (range (:x rect) (+ (:x rect) (:w rect)))
+        y (range (:y rect) (+ (:y rect) (:h rect)))]
+    [x y]))
+
 (defn paint-rect [grid rect]
-  (let [rect-coords (for [x (range (:x rect) (+ (:x rect) (:w rect)))
-                          y (range (:y rect) (+ (:y rect) (:h rect)))]
-                      [x y])]
-    (reduce (fn [grid' coord]
-              (update grid' coord #(inc (or % 0))))
-            grid
-            rect-coords)))
+  (reduce (fn [grid' coord]
+            (update grid' coord #(inc (or % 0))))
+          grid
+          (rect-coords rect)))
 
 (defn paint-grid [rects]
   (reduce paint-rect {} rects))
 
 (defn count-grid [grid]
   (count (filter #(> % 1) (vals grid))))
+
+(defn all-ones? [grid rect]
+  (every? #(= % 1) (map grid (rect-coords rect))))
 
 (def INPUT-PATTERN #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)\n")
 
@@ -30,4 +36,4 @@
         rects (map get-rect entries)
         grid (paint-grid rects)]
     [(count-grid grid)
-     0]))
+     (:id (first (filter #(all-ones? grid %) rects)))]))
