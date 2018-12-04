1f9e87ad9c598843f246063e63cbe83f0debe8f1
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-04 20:57:40 +0100

    Oops, fix error in test.

--- test/aoc2018/day03_test.clj
+++ test/aoc2018/day03_test.clj
@@ -5,7 +5,7 @@
 (deftest get-rect-test
   (testing "get-rect"
     (let [input-match (first (re-seq INPUT-PATTERN "#0 @ 1,2: 3x4\n"))]
-      (is (= {:x 1, :y 2, :w 3, :h 4} (get-rect input-match))))))
+      (is (= {:id 0, :x 1, :y 2, :w 3, :h 4} (get-rect input-match))))))
 
 (deftest paint-rect-test
   (testing "paint-rect"
