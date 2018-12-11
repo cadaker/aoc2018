0e409e22b233096cb1f72c9218b49e9baab94bcf
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-11 09:28:07 +0100

    Day 11 part 2.

--- src/aoc2018/day11.clj
+++ src/aoc2018/day11.clj
@@ -24,6 +24,34 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn integral-image [func w h]
+  (let [xys (for [x (range 0 (inc w)), y (range 0 (inc h))]
+              [x y])
+        int-img (reduce (fn [img [x y]]
+                          (assoc img
+                            [x y]
+                            (if (or (zero? x) (zero? y))
+                              0
+                              (+' (func [x y])
+                                  (- (img [(dec x) (dec y)]))
+                                  (img [x (dec y)])
+                                  (img [(dec x) y])))))
+                        {}
+                        xys)]
+    int-img))
+
+(defn img-grid-sum [img w h [x' y']]
+  (let [x (dec x')
+        y (dec y')
+        xw (+ x w)
+        yh (+ y h)]
+    (+ (img [xw yh])
+       (img [x y])
+       (- (img [xw y]))
+       (- (img [x yh])))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def serial-number 7989)
 
 (def width 300)
@@ -37,4 +65,13 @@
   [(let [xys (for [x (range 1 (- width 1)), y (range 1 (- height 1))]
                [x y])]
      (apply max-key (partial power-of-grid serial-number 3 3) xys))
-   0])
+   (let [xyss (for [size (range 1 (inc (min width height)))
+                    x (range 1 width)
+                    y (range 1 height)
+                    :when (and (<= (+ x size) width)
+                               (<= (+ y size) height))]
+                [x y size])
+         int-img (integral-image (partial power-level serial-number) width height)]
+     (apply max-key (fn [[x y size]]
+                      (img-grid-sum int-img size size [x y]))
+            xyss))])
--- test/aoc2018/day11_test.clj
+++ test/aoc2018/day11_test.clj
@@ -16,3 +16,19 @@
     (is (= 0 (power-level 39 [217 196])))
     (is (= 4 (power-level 71 [101 153])))
     ))
+
+(deftest int-img-test
+  (testing "int-img"
+    (is (= (integral-image (partial apply +) 3 3)
+           {[0 0] 0, [1 0] 0, [2 0] 0,  [3 0] 0
+            [0 1] 0, [1 1] 2, [2 1] 5,  [3 1] 9,
+            [0 2] 0, [1 2] 5, [2 2] 12, [3 2] 21,
+            [0 3] 0, [1 3] 9, [2 3] 21, [3 3] 36}))
+    (let [img (integral-image (partial power-level serial-number) 300 300)]
+      (is (= (power-of-grid serial-number 1 1 [1 1])
+             (img-grid-sum img 1 1 [1 1])))
+      (is (= (power-of-grid serial-number 4 4 [2 3])
+             (img-grid-sum img 4 4 [2 3])))
+      (is (= (power-of-grid serial-number 3 3 [17 23])
+             (img-grid-sum img 3 3 [17 23])))
+      )))
