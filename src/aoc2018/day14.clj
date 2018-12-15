46a1ed5d8fc12e8c71244482c943b829c8266e84
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-15 01:52:55 +0100

    Remove some unused code.

--- src/aoc2018/day14.clj
+++ src/aoc2018/day14.clj
@@ -6,14 +6,6 @@
   {0 [0] 1 [1] 2 [2] 3 [3] 4 [4] 5 [5] 6 [6] 7 [7] 8 [8] 9 [9] 10 [1 0]
    11 [1 1] 12 [1 2] 13 [1 3] 14 [1 4] 15 [1 5] 16 [1 6] 17 [1 7] 18 [1 8]})
 
-(defn digits-of* [n]
-  (if (zero? n)
-    (list 0)
-    (loop [n n, digits ()]
-      (if (zero? n)
-        digits
-        (recur (quot n 10) (cons (rem n 10) digits))))))
-
 (defn step [[recipes elf1 elf2]]
   (let [recipe1 (nth recipes elf1)
         recipe2 (nth recipes elf2)
@@ -28,24 +20,6 @@
 (defn steps-after [n]
   (let [recipes (ffirst (drop-while #(< (count (first %)) (+ n 10)) steps))]
     (take 10 (drop n recipes))))
-
-(def indexes (iterate inc 0))
-
-(defn find-subseq-index [xs recipes]
-  (let [parts (partition (count xs) 1 recipes)
-        indexed-parts (map-indexed vector parts)
-        matching-parts (filter #(= xs (second %)) indexed-parts)]
-    (if (seq matching-parts)
-      (ffirst matching-parts)
-      nil)))
-
-(defn search-subseq* [xs]
-  (loop [n 256, steps steps]
-    (println n)
-    (let [steps' (drop n steps)]
-      (if-let [ix (find-subseq-index xs (ffirst steps'))]
-        ix
-        (recur (* n 2) steps')))))
 
 (defn safe-subvec [xs start end]
   (if (or (neg? start)
--- test/aoc2018/day14_test.clj
+++ test/aoc2018/day14_test.clj
@@ -34,15 +34,6 @@
     (is (= '(5 9 4 1 4 2 9 8 8 2) (steps-after 2018)))
     ))
 
-(deftest find-subseq-index-test
-  (testing "find-subseq-index"
-    (is (= 9 (find-subseq-index '(5 1 5 8 9) (first (nth steps 100)))))
-    (is (= nil (find-subseq-index '(5 1 5 8 9) (first (nth steps 4)))))
-    (is (= 5 (find-subseq-index '(0 1 2 4 5) (first (nth steps 100)))))
-    (is (= 18 (find-subseq-index '(9 2 5 1 0) (first (nth steps 100)))))
-    (is (= 2018 (find-subseq-index '(5 9 4 1 4) (first (nth steps 2200)))))
-    ))
-
 (deftest search-subseq-test
   (testing "search-subseq"
     (is (= 9 (search-subseq '(5 1 5 8 9))))
