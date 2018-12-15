34b98855a740db5173b497cbdd5974d68c59d553
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-15 13:31:30 +0100

    Day 15, proper movement.

--- src/aoc2018/day15.clj
+++ src/aoc2018/day15.clj
@@ -1,11 +1,13 @@
 (ns aoc2018.day15
   (:use aoc2018.driver))
 
+(def HP 200)
+
 (defn read-map-line [[cave units] [row line]]
   (reduce (fn [[cave units] col]
             (case (get line col)
-              \G [(assoc cave [row col] \.) (assoc units [row col] {:type :goblin :hp 200})]
-              \E [(assoc cave [row col] \.) (assoc units [row col] {:type :elf :hp 200})]
+              \G [(assoc cave [row col] \.) (assoc units [row col] {:type :goblin :hp HP})]
+              \E [(assoc cave [row col] \.) (assoc units [row col] {:type :elf :hp HP})]
               \. [(assoc cave [row col] \.) units]
               \# [(assoc cave [row col] \#) units]))
           [cave units]
@@ -16,12 +18,19 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn in-range? [cave units [row col]]
-  (and (= \. (cave [row col])) (nil? (units [row col]))))
+(defn valid? [cave [row col]]
+  (and (= \. (cave [row col]))))
 
-(defn neighbours [cave units [row col]]
+(defn in-range? [cave units pos]
+  (and (valid? cave pos) (nil? (units pos))))
+
+(defn all-neighbours [cave [row col]]
+  "Return the valid (non-wall) neighbour coordinates of the point"
+  (filter (partial valid? cave) (list [(dec row) col] [row (dec col)] [row (inc col)] [(inc row) col])))
+
+(defn neighbours [cave units pos]
   "Return the in-range neighbour coordinates of the point, in reading order."
-  (filter (partial in-range? cave units) (list [(dec row) col] [row (dec col)] [row (inc col)] [(inc row) col])))
+  (filter (partial in-range? cave units) (all-neighbours cave pos)))
 
 (defn target-positions [cave units unit-type [row col]]
   "Return a set of all the positions where a unit of type unit-type at [row col] would want to go."
@@ -48,9 +57,37 @@
                 :else
                 (let [new-steps (neighbours cave units [r c])]
                   (recur (reduce conj queue' new-steps)
-                         (into initial-direction (for [s new-steps]
+                         (into initial-direction (for [s new-steps
+                                                       :when (nil? (initial-direction s))]
                                                    [s (initial-direction [r c])]))
                          (conj visited? [r c])))))))))
+
+(defn neighbour-target [cave units unit-type pos]
+  (let [target? (fn [rc]
+                  (and (units rc)
+                       (not= unit-type (:type (units rc)))))]
+    (first (filter target? (all-neighbours cave pos)))))
+
+(defn move-unit-to [units start-pos end-pos]
+  (assoc (dissoc units start-pos) end-pos (units start-pos)))
+
+(defn do-round [cave units]
+  (loop [actions (sort-by key units)
+         units units]
+    (if (empty? actions)
+      units
+      (let [[pos {unit-type :type}] (first actions)
+            close-target (neighbour-target cave units unit-type pos)
+            new-pos (find-move cave units unit-type pos)]
+        (cond close-target
+              ;; TODO: attack
+              (recur (rest actions) units)
+              ;; Move to a new spot
+              new-pos
+              (recur (rest actions) (move-unit-to units pos new-pos))
+              ;; No reachable target
+              :else
+              (recur (rest actions) units))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
--- test/aoc2018/day15_test.clj
+++ test/aoc2018/day15_test.clj
@@ -16,9 +16,9 @@
                [2 0] \# [2 1] \. [2 2] \. [2 3] \. [2 4] \. [2 5] \. [2 6] \# 
                [3 0] \# [3 1] \. [3 2] \. [3 3] \. [3 4] \. [3 5] \. [3 6] \# 
                [4 0] \# [4 1] \# [4 2] \# [4 3] \# [4 4] \# [4 5] \# [4 6] \#}
-              {[1 2] {:type :goblin :hp 200} [1 4] {:type :elf :hp 200}
-               [2 1] {:type :elf :hp 200} [2 3] {:type :goblin :hp 200} [2 5] {:type :elf :hp 200}
-               [3 2] {:type :goblin :hp 200} [3 4] {:type :elf :hp 200}}])))))
+              {[1 2] {:type :goblin :hp HP} [1 4] {:type :elf :hp HP}
+               [2 1] {:type :elf :hp HP} [2 3] {:type :goblin :hp HP} [2 5] {:type :elf :hp HP}
+               [3 2] {:type :goblin :hp HP} [3 4] {:type :elf :hp HP}}])))))
 
 (let [test-input (read-map "#######\n#E..G.#\n#...#.#\n#.G.#G#\n#######\n")]
   (def test-cave-1 (first test-input))
@@ -40,4 +40,40 @@
   (testing "find-move"
     (is (= (find-move test-cave-1 test-units-1 :elf [1 1]) [1 2]))
     (let [[cave units] (read-map "#######\n#.E...#\n#.....#\n#...G.#\n#######\n")]
-      (is (= (find-move cave units :elf [1 2]) [2 2])))))
+      (is (= (find-move cave units :elf [1 2]) [1 3])))
+    (let [[cave units] (read-map "#####\n#G..#\n#...#\n#..E#\n#####\n")]
+      (is (= (find-move cave units :goblin [1 1]) [1 2])))))
+
+(let [test-input (read-map
+"#########
+#G..G..G#
+#.......#
+#.......#
+#G..E..G#
+#.......#
+#.......#
+#G..G..G#
+#########")]
+  (def test-cave-2 (first test-input))
+  (def test-units-2 (second test-input)))
+
+(deftest neighbour-target-test
+  (testing "neighbour-target"
+    (is (= (neighbour-target test-cave-2 {[2 4] {:type :goblin :hp HP} [3 4] {:type :elf :hp HP}} :goblin [2 4])
+           [3 4]))))
+
+(deftest movement-test
+  (testing "movement-test"
+    (let [rounds (iterate (partial do-round test-cave-2) test-units-2)
+          G (fn [hp] {:type :goblin :hp hp})
+          E (fn [hp] {:type :elf :hp hp})]
+      (is (= (nth rounds 1)
+             {[1 2] (G HP) [1 6] (G HP) [2 4] (G HP) [3 4] (E HP) [3 7] (G HP)
+              [4 2] (G HP) [6 1] (G HP) [6 4] (G HP) [6 7] (G HP)}))
+      (is (= (nth rounds 2)
+             {[1 3] (G HP) [1 5] (G HP) [2 4] (G HP) [3 4] (E HP) [3 6] (G HP)
+              [3 2] (G HP) [5 1] (G HP) [5 4] (G HP) [5 7] (G HP)}))
+      (is (= (nth rounds 3)
+             {[2 3] (G HP) [2 4] (G HP) [2 5] (G HP) [3 4] (E HP) [3 5] (G HP)
+              [3 3] (G HP) [4 1] (G HP) [4 4] (G HP) [5 7] (G HP)}))
+      )))
