970f21260f593e903c570c6f6a67165e6803d6b1
Christian Adåker <cadaker@users.noreply.github.com>
2018-12-21 18:30:14 +0100

    Day 21 part 2.

--- src/aoc2018/day21.clj
+++ src/aoc2018/day21.clj
@@ -1,5 +1,5 @@
 (ns aoc2018.day21
-  (:use aoc2018.driver))
+  (:use aoc2018.driver aoc2018.cycle))
 
 (defn inner-loop [R3 R4]
   (let [R4* (-> R4
@@ -11,11 +11,16 @@
       R4*
       (recur (quot R3 256) R4*))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (def R3-init 65536)
 (def R4-init 10283511)
 
+(defn outer-loop [R4]
+  (inner-loop (bit-or R4 65536) R4-init))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defsolution day21 [_]
   [(inner-loop R3-init R4-init)
-   0])
+   (let [steps (iterate outer-loop 0)
+         [i j] (aoc2018.cycle/find-cycle steps)]
+     (nth steps (dec j)))])
